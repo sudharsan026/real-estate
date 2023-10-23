35ec13ead00b0bdf17789f868b0b5c47887b6748
Sudharsan <sudharsan.ravichandran@whatsplan.app>
2023-10-23 23:28:14 +0530

    Create a middleware and a function to handle possible error

---
+++ api/utils/error.js
@@ -0,0 +1,7 @@
+//TO throw error manually
+export const errorHandler = (statusCode, message) => {
+  const error = new Error();
+  error.statusCode = statusCode;
+  error.message = message;
+  return error;
+};
