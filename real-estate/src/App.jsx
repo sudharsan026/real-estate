5cc0f869126090fe2aa4af3f1f23f07cc2aacf2d
Sudharsan <sudharsan.ravichandran@whatsplan.app>
2023-10-23 23:44:28 +0530

    Complete signup page ui

--- real-estate/src/App.jsx
+++ real-estate/src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
-import SignOut from "./pages/SignOut";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
@@ -15,7 +15,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
