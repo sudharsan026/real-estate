33397180a41869aeae7e1a973abca72439a44b53
Sudharsan <sudharsan.ravichandran@whatsplan.app>
2023-10-23 15:31:08 +0530

    Create signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -15,6 +16,8 @@ mongoose
   });
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server runs at 3000!!");
 });
@@ -22,3 +25,4 @@ app.listen(3000, () => {
 //req is the data get from our client side and res is the data from our server to client
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
