4f8df5fb0009c95a363a19b6cab0c6ca65b7d1e5
Sudharsan <sudharsan.ravichandran@whatsplan.app>
2023-10-29 15:37:28 +0530

    Complete profile page ui

--- real-estate/src/pages/Profile.jsx
+++ real-estate/src/pages/Profile.jsx
@@ -1,7 +1,44 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="p-3 max-w-lg m-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          className="h-24 w-24 rounded-full object-cover cursor-pointer self-center mt-2"
+          src={currentUser.avatar}
+          alt="profile"
+        />
+        <input
+          type="text"
+          id="username"
+          placeholder="username"
+          className="p-3 border rounded-lg"
+        />
+        <input
+          type="text"
+          id="email"
+          placeholder="email"
+          className="p-3 border rounded-lg"
+        />
+        <input
+          type="text"
+          id="password"
+          placeholder="password"
+          className="p-3 border rounded-lg"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          update
+        </button>
+      </form>
+      <div className="flex justify-between mt-4">
+        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span className="text-red-700 cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
